1acd5dddbc74ebc5f820bfc6c59c32848793b436
Vitaliy Gunbin <vitalik_gunbin@mail.ru>
2023-01-30 19:54:53 +0300

    add Example005

---
+++ Example005_if-else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "виталий")
+{
+    Console.WriteLine("Здарова чухан!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
