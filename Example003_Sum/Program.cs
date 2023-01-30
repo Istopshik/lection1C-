bab28c9a4747b99cd04ea2a26d124856ce49114e
Vitaliy Gunbin <vitalik_gunbin@mail.ru>
2023-01-30 19:33:47 +0300

    Add random numbers in Example003_sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = 55;
-int numberB = 88;
+﻿int numberA = new Random().Next(1,10); // Случайное рандомное число от 1 до 9
+int numberB = new Random().Next(1,88);
 int result = numberA + numberB;
 Console.WriteLine(result);
